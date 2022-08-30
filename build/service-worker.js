9305370b6ade9f8e8efcaaf9979a6d052bd38913
Marionogame <Mario19ok@gmail.com>
2022-08-29 22:28:53 -0400

    config add

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marionogameportfolio/precache-manifest.209366112f1a56cbfd5f0a157884a5e1.js"
+  "/marionogamePortafolio.github.io/precache-manifest.6d25631ce81192689b127092ed30fbc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marionogameportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marionogamePortafolio.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
